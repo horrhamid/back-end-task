83f56e28760b95061db6a16b11abae45836fe8b1
hamidreza sadeghi <xhamid0011@gmail.com>
2022-12-27 01:45:04 +0330

    database Cleaned

--- backend_hiring_task/apps/urls.py
+++ backend_hiring_task/apps/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import AppsView, AppView, AppRunView
+from .views import AppsView, AppView, AppRunView, AppHistoryView
 
 urlpatterns = [
     path('apps/', AppsView.as_view()),
     path('apps/<int:app_id>/', AppView.as_view()),
     path('apps/<int:app_id>/run/', AppRunView.as_view()),
+    path('apps/<int:app_id>/history/', AppHistoryView.as_view()),
 ]
--- backend_hiring_task/apps/views.py
+++ backend_hiring_task/apps/views.py
@@ -1,8 +1,7 @@
 from .serializers import AppsSerializer, DictySerializer
-from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
-import os
+import docker
 from .models import Apps, Dicty
 
 
@@ -27,12 +26,12 @@ class AppsView(APIView):
                 else:
                     return Response(dict_ser.errors, 400)
 
-        return Response(data={"status": "done"})
+        return Response({"status": "done"})
 
-    def get(self, request):
+    def get(self, request, **kwargs):
         app = Apps.objects.all()
-        data = [a.name for a in app]
         ser = AppsSerializer(app, many=True)
+
         return Response(ser.data)
 
 
@@ -40,22 +39,71 @@ class AppView(APIView):
 
     def get(self, request, app_id, **kwargs):
         app = Apps.objects.filter(id=app_id)
-        data = [a.name for a in app]
         ser = AppsSerializer(app, many=True)
+
         return Response(ser.data)
+
+    def delete(self, request, app_id, **kwargs):
+        app = Apps.objects.filter(id=app_id)
+        app.delete()
+
+        return Response("App deleted!")
+
+    def put(self, request, app_id, **kwargs):
+        app = Apps.objects.get(pk=app_id)
+        data = request.data
+        apps_ser = AppsSerializer(app, data={"name": data["name"], "image": data["image"], "command": data["command"]})
+        if apps_ser.is_valid():
+            apps_ser.save()
+            return Response(apps_ser.data)
+        else:
+            return Response(apps_ser.errors, 400)
+
+
 
 
 class AppRunView(APIView):
-    def post(self, request, app_id, **kwargs):
+
+    def post(request, app_id, **kwargs):
         app = Apps.objects.filter(id=app_id)[0]
         envs = Dicty.objects.filter(app_id=app.pk)
-        command = "docker  run -d"
-        for env in envs:
-            command += " -e " + env.key + "=" + env.value
+        client = docker.from_env()
+        environments = {}
 
-        command += " --name " + app.name + "_v" + str(app.version) + " " + app.image + " " + app.command
+        for env in envs:
+            environments[env.key] = env.value
+
+        container = client.containers.run(app.image, app.command, name=(app.name + "__v" + str(app.version)),
+                                          labels=environments, environment=environments, detach=True)
         app.version = app.version + 1
         app.save()
-        return_value = os.system(command)
-        return Response(str(return_value) + " " + command)
+        return Response("Done!")
 
+
+class AppHistoryView(APIView):
+
+    def post(request, app_id, **kwargs):
+        app = Apps.objects.filter(id=app_id)[0]
+        app_name = app.name
+        client = docker.from_env()
+        containers_list = client.containers.list(all=True)
+
+        context = {}
+
+        for container in containers_list:
+            temp = {}
+            if container.attrs["Name"].split("__v")[0][1:] == app_name:
+                temp["Name"] = container.attrs["Name"][1:]
+                temp["Status"] = container.attrs["State"]["Status"]
+                temp["Started"] = container.attrs["State"]["StartedAt"]
+                if temp["Status"] == "running":
+                    temp["Finished"] = "-1"
+                else:
+                    temp["Finished"] = container.attrs["State"]["FinishedAt"]
+                temp["Image"] = container.attrs["Config"]["Image"]
+                temp["Envs"] = container.attrs["Config"]["Env"]
+                temp["Args"] = container.attrs["Args"]
+
+                context[container.id] = temp
+
+        return Response(context)
